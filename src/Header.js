c95d130a4a2855e97e630eb6cc34b03f7dfc926d
amarchuk <p07a1330@ya.ru>
2020-04-08 12:12:20 +0300

    .

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Header.css"
+const Header = ()=>{
+    return (
+        <div></div>
+    )  ;
+};
+
+export default  Header;
---
+++ src/Scroll.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './Scroll.css'
+const Scroll = ()=>{
+    return (
+        <div>Scroll</div>
+    )
+}
+
+export default  Scroll;
