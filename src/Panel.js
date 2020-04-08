b9badef7aeacfd6da12a33859641936f6e4c8e6e
amarchuk <p07a1330@ya.ru>
2020-04-08 14:11:46 +0300

    Workable

--- src/Panel.js
+++ src/Panel.js
@@ -2,5 +2,42 @@ import React from "react";
 import "./Panel.css"
 
 let Panel = (props)=>{
+    let id= props.id;
+    let image= props.image;
+    let date= props._date;
+    let sity= props.sity;
+    let descript= props.descript;
+    let visitors= props.visitors;
+    let speakers= props.speakers;
+    let price= props.price;
 
+    return (
+        <div className = {"Panel"}>
+            <div>
+                <img src={image} className={"Panel-image"} height={window.innerHeight*0.12}/>
+            </div>
+            <div className={"Panel-sity"}> {sity} </div>
+            <hr width={"15%"} align={"center"} color={"green"} size={"5px"} />
+            <div className={"Panel-descript"}> {descript}</div>
+            <div className={"Panel-foot"}>
+                <div className={"Panel-foot-block"}>
+                    <img src={"http://localhost/img/users.png"} className={"Panel-foot-block-image"}/>
+                    {visitors}
+                </div>
+                <div className={"Panel-foot-block"}>
+                    <img src={"http://localhost/img/user.jpg"} className={"Panel-foot-block-image"}/>
+                    {speakers}
+                </div>
+                <div className={"Panel-foot-block"}>
+                    <img src={"http://localhost/img/coin.jpg"}
+                        className={"Panel-foot-block-image"}/>
+                    {price}
+                </div>
+
+
+            </div>
+        </div>
+    )
 };
+
+export default Panel;
--- src/Scroll.js
+++ src/Scroll.js
@@ -1,9 +1,23 @@
 import React from "react";
 import './Scroll.css'
+import Panel from "./Panel";
 const Scroll = ()=>{
     return (
-        <div className={"Scroll"}>Scroll</div>
-    )
+       <div className={"Scroll"}>
+           <Panel
+
+               id =" 12345  "
+               image =" https://ptfair.ru/netcat_files/1/183/DSC_0159_0.JPG  "
+               _date =" 22-26 февраля 2016  "
+               sity =" г. Уфа  "
+               descript =" Машиностроение  промышленные насосы и клапаны  Гидравлика и пневматика  промышленное оборудование  "
+               visitors =" >5000  "
+               speakers =" 74  "
+               price =" 12000 "></Panel>
+
+
+       </div>
+           )
 }
 
 export default  Scroll;
